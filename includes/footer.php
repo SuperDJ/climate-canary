c3223d09dc52a7da7d7ebf809979eebe2fcd8a43
Maaike <maaike_pastoor@hotmail.com>
2017-03-31 13:41:32 +0200

    Bootstrap toegevoegd Shit aan het proberen in de index Yolo

--- includes/footer.php
+++ includes/footer.php
@@ -2,5 +2,6 @@
 
 		<script src="/climate-canary/js/vendor.min.js"></script>
 		<script src="/climate-canary/js/app.min.js"></script>
+		<script src="/climate-canary/js/bootstrap.min.js"></script>
 	</body>
 </html>

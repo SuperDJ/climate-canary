4e2ab37c0f7ea83f38f774fb7c048b4243d3d67b
SuperDJ <super.derkjan@gmail.com>
2017-03-31 16:46:41 +0200

    Fixed index

--- index.php
+++ index.php
@@ -3,64 +3,54 @@ $title = 'Home';
 require_once $_SERVER['DOCUMENT_ROOT'].'/includes/header.php';
 ?>
 
-<section class="container">
-    <div class="row">
-        <div class="col .col-xs-4 .col-sm-6">
-            <a href="#" class="card center home">
-                <div class="card-supporting">
-                    <i class="material-icons">home</i>
-                    <span>Naar huis</span>
-                </div>
-            </a>
-        </div>
+<section class="row">
+    <div class="col col-xs-4 col-sm-6">
+        <a href="#" class="sc-card center home">
+            <div class="sc-card-supporting">
+                <i class="material-icons">home</i>
+                <span>Naar huis</span>
+            </div>
+        </a>
     </div>
 
 
-    <div class="row">
-        <div class="col .col-xs-4 .col-sm-6"">
-            <a href="#" class="card center home">
-                <div class="card-supporting">
-                    <i class="material-icons">navigation</i>
-                    <span>Navigeer naar</span>
-                </div>
-            </a>
-        </div>
+    <div class="col col-xs-4 col-sm-6">
+        <a href="#" class="sc-card center home">
+            <div class="sc-card-supporting">
+                <i class="material-icons">navigation</i>
+                <span>Navigeer naar</span>
+            </div>
+        </a>
     </div>
 
-    <div class="row">
-        <div class="col .col-xs-4 .col-sm-6"">
-            <a href="#" class="card center home">
-                <div class="card-supporting">
-                    <i class="material-icons">wb_sunny</i>
-                    <span>Temperatuur</span>
-                </div>
-            </a>
-        </div>
+    <div class="col col-xs-4 col-sm-6">
+        <a href="#" class="sc-card center home">
+            <div class="sc-card-supporting">
+                <i class="material-icons">wb_sunny</i>
+                <span>Temperatuur</span>
+            </div>
+        </a>
     </div>
 
-    <div class="row">
-        <div class="col .col-xs-4 .col-sm-6"">
-            <a href="#" class="card center home">
-                <div class="card-supporting">
-                    <i class="material-icons">grain</i>
-                    <span>CO<sub>2</sub> gehalte</span>
-                </div>
-            </a>
-        </div>
+    <div class="col col-xs-4 col-sm-6">
+        <a href="#" class="sc-card center home">
+            <div class="sc-card-supporting">
+                <i class="material-icons">grain</i>
+                <span>CO<sub>2</sub> gehalte</span>
+            </div>
+        </a>
     </div>
 
-    <div class="row">
-        <div class="col .col-xs-4 .col-sm-6"">
-            <a href="#" class="card center home">
-                <div class="card-supporting">
-                    <i class="material-icons">cloud</i>
-                    <span>Luchtvochigheid</span>
-                </div>
-            </a>
-        </div>
+    <div class="col col-xs-4 col-sm-6">
+        <a href="#" class="sc-card center home">
+            <div class="sc-card-supporting">
+                <i class="material-icons">cloud</i>
+                <span>Luchtvochigheid</span>
+            </div>
+        </a>
     </div>
 
-    <div class="sc-col sc-xs2 sc-s4">
+    <div class="col col-xs-4 col-sm-6">
         <a href="#" class="sc-card sc-center home">
             <div class="sc-card-supporting">
                 <i class="material-icons">settings</i>

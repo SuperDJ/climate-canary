b80e06e8d100f9ec5b57c289dedfab2e6e9a4390
SuperDJ <super.derkjan@gmail.com>
2017-04-04 20:56:49 +0200

    Continued on route confirmation

--- navigation-confirm.php
+++ navigation-confirm.php
@@ -8,6 +8,7 @@ $fLng = $db->sanitize( $_GET['fromLng'] );
 $to = $db->sanitize( $_GET['to'] );
 $tLat = $db->sanitize( $_GET['toLat'] );
 $tLng = $db->sanitize( $_GET['toLng'] );
+
 if( !empty( $from ) && !empty( $fLat ) && !empty( $fLng ) && !empty( $to ) && !empty( $tLat ) && !empty( $tLng ) ) {
 ?>
 	<section class="row">
@@ -15,7 +16,7 @@ if( !empty( $from ) && !empty( $fLat ) && !empty( $fLng ) && !empty( $to ) && !e
 	</section>
 
 	<section class="row details">
-		<div class="col col-xs-8 col-sm-8">
+		<div class="col col-xs-7">
 			<div class="row">
 				<div class="col col-xs-12">
 					<p>Naar <?php echo $to; ?></p>
@@ -28,8 +29,14 @@ if( !empty( $from ) && !empty( $fLat ) && !empty( $fLng ) && !empty( $to ) && !e
 			</div>
 		</div>
 
-		<div class="col col-xs-4 col-sm-4">
+		<div class="col col-xs-5">
+            <div class="col col-xs-12">
+                <a href="/climate-canary/navigate-to.php" class="sc-raised-button grey"><!--<i class="material-icons">arrow_back</i>--> Terug</a>
+            </div>
 
+            <div class="col col-xs-12">
+                <a href="/climate-canary/navigate.php" class="sc-raised-button"><!--<i class="material-icons">navigation</i>-->Start</a>
+            </div>
 		</div>
 	</section>
 
@@ -43,11 +50,7 @@ if( !empty( $from ) && !empty( $fLat ) && !empty( $fLng ) && !empty( $to ) && !e
 			});
 			directionsDisplay.setMap(map);
 
-			//var onChangeHandler = function() {
-				calculateAndDisplayRoute(directionsService, directionsDisplay);
-		/*	};
-			document.getElementById('start').addEventListener('change', onChangeHandler);
-			document.getElementById('end').addEventListener('change', onChangeHandler);*/
+            calculateAndDisplayRoute(directionsService, directionsDisplay);
 		}
 
 		function calculateAndDisplayRoute(directionsService, directionsDisplay) {
@@ -63,6 +66,45 @@ if( !empty( $from ) && !empty( $fLat ) && !empty( $fLng ) && !empty( $to ) && !e
 				}
 			});
 		}
+
+		// Create the XHR object.
+		function createCORSRequest( method, url ) {
+			var xhr = new XMLHttpRequest();
+			if( 'withCredentials' in xhr ) {
+				// XHR for Chrome/Firefox/Opera/Safari.
+				xhr.open( method, url, true );
+			} else if( typeof XDomainRequest != "undefined" ) {
+				// XDomainRequest for IE.
+				xhr = new XDomainRequest();
+				xhr.open(method, url);
+			} else {
+				// CORS not supported.
+				xhr = null;
+			}
+			return xhr;
+		}
+
+		function getRouteDetails( fLat, fLng, tLat, tLng ) {
+			var url = 'https://maps.googleapis.com/maps/api/directions/json?origin='+fLat+','+fLng+'&destination='+tLat+','+tLng+'&key=AIzaSyAB9kYP7clJyhX45mt6y3LobeKA9L6ivNo',
+			    xhr = createCORSRequest('GET', url);
+
+			if( !xhr ) {
+				console.log('CORS not supported');
+				return
+            }
+
+            xhr.onload = function() {
+				var data = xhr.responseText;
+				console.log(data);
+            }
+
+            xhr.onerror = function() {
+				console.log('There was an error making the request');
+            }
+
+            xhr.send();
+        }
+        getRouteDetails(<?php echo $fLat.', '.$fLng.', '.$tLat.', '.$tLng; ?>);
 	</script>
 	<script async defer
 			src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAB9kYP7clJyhX45mt6y3LobeKA9L6ivNo&callback=initMap">

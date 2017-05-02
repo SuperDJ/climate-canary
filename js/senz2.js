ad585f3ea2fd55c786f40a1fc3789a632d2ca4eb
SuperDJ <super.derkjan@gmail.com>
2017-05-02 08:23:35 +0200

    Fixed Senz2 API

--- js/senz2.js
+++ js/senz2.js
@@ -45,11 +45,29 @@ $(document).ready(function() {
 	}
 
 	function getSensorData( token, fn ) {
-		var to = new Date(),
-			from = new Date( to - 60000*15 ),
-			to = to.getFullYear()+'-'+( to.getMonth() < 10 ? '0'+to.getMonth() : to.getMonth() )+'-'+( to.getDate() < 10 ? '0'+to.getDate() : to.getDate() )+( to.getHours() < 10 ? '0'+to.getHours() : to.getHours() )+':'+( to.getMinutes() < 10 ? '0'+to.getMinutes() : to.getMinutes() )+':'+( to.getSeconds() < 10 ? '0'+to.getSeconds() : to.getSeconds() ),
-			from = from.getFullYear()+'-'+( from.getMonth() < 10 ? '0'+from.getMonth() : from.getMonth() )+'-'+( from.getDate() < 10 ? '0'+from.getDate() : from.getDate() )+( from.getHours() < 10 ? '0'+from.getHours() : from.getHours() )+':'+( from.getMinutes() < 10 ? '0'+from.getMinutes() : from.getMinutes() )+':'+( from.getSeconds() < 10 ? '0'+from.getSeconds() : from.getSeconds() ),
-			url = 'https://apiv1.makesenz2.nl/api/sensor/16/data/'+from+'/'+to;
+		var id = '20',
+			minutes = 30,
+			to = new Date(),
+			from = new Date( to - 60000 * minutes ),
+			to = to.getUTCFullYear() + '-' +
+				( ( to.getUTCMonth() + 1 ) < 10 ? '0' + ( to.getUTCMonth() + 1 ) : ( to.getUTCMonth() + 1 ) ) + '-' +
+				( to.getUTCDate() < 10 ? '0' + to.getUTCDate() : to.getUTCDate() ) +
+				( to.getUTCHours() < 10 ? '0' + to.getUTCHours() : to.getUTCHours() ) + ':' +
+				( to.getUTCMinutes() < 10 ? '0' + to.getUTCMinutes() : to.getUTCMinutes() ) + ':' +
+				( to.getUTCSeconds() < 10 ? '0' + to.getUTCSeconds() : to.getUTCSeconds() ),
+			from = from.getUTCFullYear() + '-' +
+				( ( from.getUTCMonth() + 1 ) < 10 ? '0' + ( from.getUTCMonth() + 1) : ( from.getUTCMonth() + 1 ) ) + '-' +
+				( from.getUTCDate() < 10 ? '0' + from.getUTCDate() : from.getUTCDate() ) +
+				( from.getUTCHours() < 10 ? '0' + from.getUTCHours() : from.getUTCHours() ) + ':' +
+				( from.getUTCMinutes() < 10 ? '0' + from.getUTCMinutes() : from.getUTCMinutes() ) + ':' +
+				( from.getUTCSeconds() < 10 ? '0'+from.getUTCSeconds() : from.getUTCSeconds() ),
+			url = 'https://apiv1.makesenz2.nl/api/sensor/'+ id +'/data/' + from + '/' + to;
+
+		/*console.log('to date:');
+		console.log(to);
+		console.log('form date');
+		console.log(from);*/
+
 		$.ajax({
 			url: url,
 			headers: {'Authorization': 'Bearer '+token.access_token},
@@ -61,16 +79,17 @@ $(document).ready(function() {
 	}
 
 	getToken( credentials, function( token ) {
-		console.log('Token:');
-		console.log( token );
+		/*console.log('Token:');
+		console.log( token );*/
 
 		getSensors( token, function( sensors ) {
-			console.log('Sensors:');
-			console.log( sensors );
+			/*console.log('Sensors:');
+			console.log( sensors );*/
 
 			getSensorData(token, function( data ) {
-		   		console.log('Data:');
-		   		console.log(data);
+		   		/*console.log('Data:');
+		   		console.log(data);*/
+		   		// Insert sensor data in database
 			});
 		});
 	});
